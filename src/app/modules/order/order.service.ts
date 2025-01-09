75fe44d2f308999e75d29ed801375c533f2e493c
SadikMukradir <sadikmuktadir2.0@gmail.com>
2025-01-09 20:16:24 +0600

    Bicycle created,get,updated anddeleted successfully

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,10 +1,25 @@
+import Bicycle from "../bicycle/bicycle.model";
 import { IOrder } from "./order.interface";
 import Order from "./order.model";
 
 const createOrder = async (payload: IOrder): Promise<IOrder> => {
+  const bicycle = await Bicycle.findById(payload.product);
+
+  if (!bicycle) {
+    throw new Error("Product not found");
+  }
+
+  if (bicycle.quantity <= 0) {
+    throw new Error("Product is out of stock");
+  }
+
   const result = await Order.create(payload);
+  bicycle.quantity -= payload.quantity;
+  await bicycle.save();
+
   return result;
 };
+
 const calculateTotalRevenue = async (): Promise<number> => {
   const pipeline = [
     {
