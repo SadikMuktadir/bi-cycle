f7f3137df35c12889acb15731cc4a63f80629afb
SadikMukradir <sadikmuktadir2.0@gmail.com>
2025-01-09 11:25:58 +0600

    Bicycle created,get,updated anddeleted successfully

--- src/app/modules/bicycle/bicycle.controller.ts
+++ src/app/modules/bicycle/bicycle.controller.ts
@@ -28,8 +28,22 @@ const getBicycle = async (req: Request, res: Response) => {
 };
 const getSingleBicycle = async (req: Request, res: Response) => {
   try {
-    const bicycleId = req.params.bicycleId;
-    const result = await bicycleService.getSingleBicycle(bicycleId);
+    const productId = req.params.productId;
+    const result = await bicycleService.getSingleBicycle(productId);
+    res.send({
+      success: true,
+      message: "Single Bicycles retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+const updateBicycle = async (req: Request, res: Response) => {
+  try {
+    const productId = req.params.productId;
+    const body = req.body;
+    const result = await bicycleService.updateBicycle(productId, body);
     res.send({
       success: true,
       message: "Single Bicycles retrieved successfully",
@@ -41,8 +55,8 @@ const getSingleBicycle = async (req: Request, res: Response) => {
 };
 const deleteBicycle = async (req: Request, res: Response) => {
   try {
-    const bicycleId = req.params.bicycleId;
-    await bicycleService.deleteBicycle(bicycleId);
+    const productId = req.params.productId;
+    await bicycleService.deleteBicycle(productId);
     res.send({
       success: true,
       message: "Bicycles deleted successfully",
@@ -55,5 +69,6 @@ export const bicycleController = {
   createBicycle,
   getBicycle,
   getSingleBicycle,
+  updateBicycle,
   deleteBicycle,
 };
--- src/app/modules/bicycle/bicycle.router.ts
+++ src/app/modules/bicycle/bicycle.router.ts
@@ -4,4 +4,7 @@ import { bicycleController } from "./bicycle.controller";
 const bicycleRouter = Router();
 bicycleRouter.post("/products", bicycleController.createBicycle);
 bicycleRouter.get("/products", bicycleController.getBicycle);
+bicycleRouter.get("/products/:productId", bicycleController.getSingleBicycle);
+bicycleRouter.patch("/products/:productId", bicycleController.updateBicycle);
+bicycleRouter.delete("/products/:productId", bicycleController.deleteBicycle);
 export default bicycleRouter;
